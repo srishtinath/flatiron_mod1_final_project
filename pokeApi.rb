9a2dd38b828b7fcdce333609f623f24cef6ca789
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-05-04 19:42:39 -0400

    put pokemon api collection script into a method

--- pokeApi.rb
+++ pokeApi.rb
@@ -1,19 +1,20 @@
 require_relative 'config/environment.rb'
 
 ##This file is used for filling Pokemon table
-
-150.times do |i|
-    data = JSON.parse(RestClient.get("https://pokeapi.co/api/v2/pokemon/#{i+1}"))
-    count = data["count"]
-    newPokemon = Pokemon.create(name:data["name"],xp:data["base_experience"],level:1)
-     types = data["types"]
-    types.length.times do |x|
-        newType = types[x]["type"]["name"]
-        myType = PokemonType.find_by(name:"#{newType}")
-        if(myType)
-            newPokemon.pokemon_types<<myType
-        else
-            newPokemon.pokemon_types<<PokemonType.create(name:"#{newType}")
+def initial_collect
+    150.times do |i|
+        data = JSON.parse(RestClient.get("https://pokeapi.co/api/v2/pokemon/#{i+1}"))
+        count = data["count"]
+        newPokemon = Pokemon.create(name:data["name"],xp:data["base_experience"],level:1)
+        types = data["types"]
+        types.length.times do |x|
+            newType = types[x]["type"]["name"]
+            myType = PokemonType.find_by(name:"#{newType}")
+            if(myType)
+                newPokemon.pokemon_types<<myType
+            else
+                newPokemon.pokemon_types<<PokemonType.create(name:"#{newType}")
+            end
         end
     end
 end
