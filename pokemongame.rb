377e83db131da9812dffd72422072e87d7b9fd86
dnamgyal <don.namgyal@hotmail.com>
2020-05-05 13:19:23 -0400

    create a new trainer from prompt

--- pokemongame.rb
+++ pokemongame.rb
@@ -6,4 +6,15 @@ prompt = TTY::Prompt.new
 
 
 prompt.say("Welcome to Pokemon World!!!")
-name = prompt.ask('What is your name?', default: ENV['USER'])
+prompt.keypress("Press enter to continue", keys: [:return])
+
+
+acct_creation = prompt.yes?('Would you like to create your trainer?')
+
+if acct_creation
+    name = prompt.ask('What is your name?')
+    age = prompt.ask('How old are you?')
+    hometown = prompt.ask('Where are you from?')
+
+    Trainer.create(name: name, age: age, hometown: hometown)
+end
