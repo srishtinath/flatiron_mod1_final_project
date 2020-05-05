224adf211bda0efd5e3c6051a975c57649fc9719
dnamgyal <don.namgyal@hotmail.com>
2020-05-05 14:38:37 -0400

    cli menus developed

--- pokemongame.rb
+++ pokemongame.rb
@@ -10,42 +10,50 @@ prompt.keypress("Press enter to continue", keys: [:return])
 
 # Create Trainer
 #Create a new Trainer instance with prompts for name, age, hometown
-acct_creation = prompt.yes?('Would you like to create your trainer?')
 
-if acct_creation
-    name = prompt.ask('What is your name?', required: true)
-    age = prompt.ask('How old are you?', required: true)
-    hometown = prompt.ask('Where are you from?', required: true)
+def trainer_creation
+    prompt = TTY::Prompt.new
+    acct_creation = prompt.yes?('Would you like to create your trainer?')
+    if acct_creation
+        name = prompt.ask('What is your name?', required: true)
+        age = prompt.ask('How old are you?', required: true)
+        hometown = prompt.ask('Where are you from?', required: true)
+    
+        #Trainer.create(name: name, age: age, hometown: hometown)
+    end
 
-    #Trainer.create(name: name, age: age, hometown: hometown)
 end
-<<<<<<< HEAD
-=======
-name = prompt.ask('What is your name?', default: ENV['USER'])
->>>>>>> 22a3827d9d569dfc8d1da7e70febe83c20c3e13d
-
-
 
 # Choose Starter Pokemon - S
     #create new instance based on choice offered (Bulbasaur, Charmander, Squirtle, Pikachu)
 
-pokemon = prompt.select("Select your starter pokemon:", ["Pikachu", "Charmander", "Squirtle", "Bulbasaur"])
-
+def starter_pokemon
+    prompt = TTY::Prompt.new
+    pokemon = prompt.select("Select your starter pokemon:", ["Pikachu", "Charmander", "Squirtle", "Bulbasaur"])
+    #add to the caught_pokemon table
+end
 
 # Now, what would you like to do?
     #Explore the town
     #Catch pokemon
     #View all pokemon
-
-<<<<<<< HEAD
-action = prompt.select("What would you like to do?", ["Explore Town", "Catch Pokemon", "View My Pokemons"])
-
-
+def starting_menu
+    prompt = TTY::Prompt.new
+    action = prompt.select("What would you like to do?", ["Explore Town", "Catch Pokemon", "View My Pokemons", "Exit Game"])
+    case action
+    when "Explore Town"
+        explore
+    when "Catch Pokemon"
+        puts "Catch Pokemon"
+    when "View My Pokemons"
+        puts "My Pokemons"
+    else
+        exit(0)
+    end
+end
 
 #Explore
-=======
 #Explore - D
->>>>>>> 22a3827d9d569dfc8d1da7e70febe83c20c3e13d
     #Professor Oak's Clinic
         #change party pokemon
     #Misty's Gym
@@ -54,23 +62,33 @@ action = prompt.select("What would you like to do?", ["Explore Town", "Catch Pok
         #learn about how game works
     #Pokemon Center
     #Police station
-    
-location = prompt.select("Where would you like to go?", ["Professor Oak's Clinic", "Misty's Gym", "Brock's House", "Pokemon Center", "Police Station", "Go Back"])
 
-case location
-when "Brock's House"
-    puts "Brock man"
-when "Professor Oak's Clinic"
-    puts "Oaky"
-when "Pokemon Center"
-    puts "Center"
-when "Police Station"
-    puts "Policia"
-when "Misty's Gym"
-    puts "Misty"
-else 
+
+def explore
+    prompt = TTY::Prompt.new
     location = prompt.select("Where would you like to go?", ["Professor Oak's Clinic", "Misty's Gym", "Brock's House", "Pokemon Center", "Police Station", "Go Back"])
+
+    case location
+    when "Brock's House"
+        puts "Brock man"
+    when "Professor Oak's Clinic"
+        puts "Oaky"
+    when "Pokemon Center"
+        puts "Center"
+    when "Police Station"
+        puts "Policia"
+    when "Misty's Gym"
+        puts "Misty"
+    else 
+        starting_menu
+    end    
+
 end
+
+
+trainer_creation
+starter_pokemon
+starting_menu
 
 
 
