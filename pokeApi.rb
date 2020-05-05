32db7514266272e988a7a01f9f3745e4223ed004
Srishti Nath <srishti.nath@gmail.com>
2020-05-05 00:06:53 -0400

    making sure latest is aligned

--- pokeApi.rb
+++ pokeApi.rb
@@ -5,22 +5,16 @@ require 'pry'
 def initial_collect(num)
     num.times do |i|
         data = JSON.parse(RestClient.get("https://pokeapi.co/api/v2/pokemon/#{i+1}"))
-<<<<<<< HEAD
         #count = data["count"]
         name = data["name"]
         xp = data["base_experience"]
         front_pic = data["sprites"]["front_default"]
         back_pic = data["sprites"]["back_default"]
-        capture_rate = JSON.parse(RestClient.get(data["species"]["url"]))["capture_rate"]
         newPokemon = Pokemon.create(name:name,xp:xp,level:1,back_img_url:back_pic,front_img_url:front_pic,capture_rate:capture_rate)
-        
-=======
-        count = data["count"]
-        newPokemon = Pokemon.create(name:data["name"],xp:data["base_experience"],level:1)
         additional_data = JSON.parse(RestClient.get("https://pokeapi.co/api/v2/pokemon-species/#{i+1}/"))
+        capture_rate = JSON.parse(RestClient.get(data["species"]["url"]))["capture_rate"]
         newPokemon.update(capture_rate: additional_data["capture_rate"])
         newPokemon.save
->>>>>>> 238983fa5bd4ac429ded26761cbea466e27be010
         types = data["types"]
         types.length.times do |x|
             newType = types[x]["type"]["name"]
