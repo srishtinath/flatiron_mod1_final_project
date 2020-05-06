9523c982fc436d020654e3b0dcccfa7fdb5c531b
dnamgyal <don.namgyal@hotmail.com>
2020-05-06 00:17:31 -0400

    can continue with previous trainer, fixed menu, added colors, and many more

--- pokemongame.rb
+++ pokemongame.rb
@@ -4,45 +4,58 @@ require_relative 'config/environment.rb'
 require_all 'app/models'
 
 prompt = TTY::Prompt.new
+# ans = prompt.select('What size?') do |menu|
+#     menu.choice 'small', 1
+#     menu.choice 'medium', 2, disabled: '(out of stock)'
+#     menu.choice 'large', 3
+# end
+# puts ans
 
-prompt.say("Welcome to Pokemon World!!!")
-prompt.keypress("Press enter to continue", keys: [:return])
+@@trainer1 = Trainer.last
+
+
+
+
+
 
 # Create Trainer
 #Create a new Trainer instance with prompts for name, age, hometown
 
 def trainer_creation
     prompt = TTY::Prompt.new
-    acct_creation = prompt.yes?('Would you like to create your trainer?') #Need to create a Trainer instance to make other methods work
-    if acct_creation
-        name = prompt.ask('What is your name?', required: true)
-        age = prompt.ask('How old are you?', required: true) #need to limit input to an int
-        hometown = prompt.ask('Where are you from?', required: true)
-    
-        @@trainer1 = Trainer.create(name: name, age: age, hometown: hometown)
-    end
-
+    puts "Please enter your account information:"
+    name = prompt.ask('What is your name?', required: true)
+    age = prompt.ask('How old are you?', required: true) #need to limit input to an int
+    hometown = prompt.ask('Where are you from?', required: true)
+    prompt.ok("Trainer succesfully created. Enjoy the game, #{name}!!!")
+    @@trainer1 = Trainer.create(name: name, age: age, hometown: hometown)
 end
 
-# Choose Starter Pokemon - S
-    #create new instance based on choice offered (Bulbasaur, Charmander, Squirtle, Pikachu)
 
-def starter_pokemon
+def find_my_trainer
     prompt = TTY::Prompt.new
-
-    starter_pokemon = prompt.select("Select your starter pokemon:", ["Pikachu", "Charmander", "Squirtle", "Bulbasaur"])
-    #add to the caught_pokemon table
-   # pokemon = Pokemon.find_by(name: pokemon)
-    #add_to_caught_pokemon(pokemon)
+    puts "Please provide us with the information used to create your trainer:"
+    name = prompt.ask('What is your name?', required: true)
+    age = prompt.ask('How old are you?', required: true) #need to limit input to an int
+    trainer_found = Trainer.find_by(name: name, age: age)
+    if trainer_found
+        prompt.ok("Welcome back #{name}!")
+        starting_menu
+    else
+        prompt.error("Sorry, no trainers were found with the information provided.")
+        prompt.error("Please try again.")
+        begin_game
+    end
 end
 
 def choose_starter
     prompt = TTY::Prompt.new
-    starter = prompt.select("Which starter pokemon would you like to choose?", %w(bulbasaur charmander squirtle pikachu))
+    starter = prompt.select("Pick your starter pokemon:", %w(Bulbasaur Charmander Squirtle Pikachu))
     starter_instance = Pokemon.find_by(name: starter)
     new_name = prompt.ask("What would you like to name your new Pokemon?")
     CaughtPokemon.create(pokemon: starter_instance, party: true, name: new_name)
-    puts "Congratulations! You just took the first step on your journey to become the greatest Pokemon master!"
+    prompt.ok("Congratulations! You just took the first step on your journey to become the greatest Pokemon master!")
+    starting_menu
 end
 
 # Now, what would you like to do?
@@ -51,8 +64,7 @@ end
     #View all pokemon
 def starting_menu
     prompt = TTY::Prompt.new
-
-    action = prompt.select("What would you like to do?", ["Explore Town", "Catch Pokemon", "View My Pokemons", "Exit Game"])
+    action = prompt.select("What would you like to do?", ["Explore Town", "Catch Pokemon", "View My Pokemons", "View All Pokemons", "Exit Game"])
     case action
     when "Explore Town"
         explore
@@ -60,6 +72,8 @@ def starting_menu
         puts "Catch Pokemon"
     when "View My Pokemons"
         puts "My Pokemons"
+    when "View All Pokemons"
+        puts "All pokemons"
     else
         abort("Game ended!")
     end
@@ -79,9 +93,7 @@ end
 
 def explore
     prompt = TTY::Prompt.new
-
     location = prompt.select("Where would you like to go?", ["Professor Oak's Clinic", "Misty's Gym", "Brock's House", "Pokemon Center", "Police Station", "Go Back"])
-
     case location
     when "Brock's House"
         puts "Brock man"
@@ -95,11 +107,31 @@ def explore
         puts "Misty"
     else 
         starting_menu
-    end    
-
+    end  
 end
 
+def begin_game
+    prompt = TTY::Prompt.new
+    choice = prompt.select("", ["Start a new game!", "Continue your game!", "Exit"], active_color: :cyan)
+    case choice
+    when "Start a new game!"
+        trainer_creation
+        choose_starter
+    when "Continue your game!"
+        find_my_trainer
+    else
+        abort("Game Exited")
+    end
+end
 
-# trainer_creation
-# starter_pokemon
-starting_menu
+##pastel.alias_color(:funky, :red, :bold)   need to install gem pastel to use colors
+#pastel.funky.on_green('unicorn')   # => will use :red, :bold color
+
+def play_game
+    prompt = TTY::Prompt.new
+    prompt.say("Welcome to Pokemon World!!!")
+    prompt.keypress("Press enter to continue", keys: [:return])
+    begin_game
+end
+
+play_game
